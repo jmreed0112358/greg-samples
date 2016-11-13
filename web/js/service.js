ce17827e26e98dc6dcfe063ee8daf6af6e4b7e87
Jeremy M. Reed <reeje76@gmail.com>
2016-11-13 00:21:15 -0700

    Clean up - Remove debug prints, change title.

--- web/js/service.js
+++ web/js/service.js
@@ -5,19 +5,14 @@ this.addEventListener("keyup", postSearchString);
 
 function postSearchString(clickEvent) {
   var searchString = document.getElementById("searchString").value;
-  console.log('clickEvent: ', clickEvent);
-  console.log('clickEvent.target.id', clickEvent.target.id);
-  console.log('clickEvent.key', clickEvent.key);
   if ((clickEvent.target.id === 'submitButton' && clickEvent.key === undefined) || 
       (clickEvent.target.id === 'submitButton' && clickEvent.key === 'Enter')) {
-    console.log('searchString: ', searchString);
     $.ajax({
       url: 'test.php',
       type: 'post',
       dataType: 'json',
       data: {input: searchString},
       success: function (result) {
-        console.log('result: ', result);
         var resultSpan = document.getElementById("resultSpan");
         resultSpan.innerHTML = result.response;
       },
--- web/test.php
+++ web/test.php
@@ -4,8 +4,6 @@ header("content-type:application/json");
 
 $data_keys = array_keys($_POST);
 
-error_log(print_r($data_keys[0], true));
-
 echo json_encode(array('response' => $_POST[$data_keys[0]]));
 
 exit();
